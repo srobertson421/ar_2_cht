fe74c3ea747d6fa241906e97c0799ffb1d36cee0
Sean Robertson <srobertson421@gmail.com>
2020-03-01 18:14:18 -0800

    Added multiline support as well as support for 022 codes

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
   const codesContainer = document.getElementById('codes');
   const newCodeNameInput = document.getElementById('new-code-name');
   const newCodeButton = document.getElementById('add-code');
-  const frontPatterns = ['001', '002', '003', '004'];
+  const frontPatterns = ['001', '002', '003', '004', '022'];
 
   function addCheat(name) {
     cheats.push({
       name: name,
-      original: '',
+      original: [],
       converted: ''
     });
 
@@ -21,22 +21,61 @@
     renderCheats();
   }
 
-  function onCodeChange(newValue, cheatIndex) {
-    cheats[cheatIndex].original = newValue;
+  function onCodeLineChange(newValue, cheatIndex, lineIndex) {
+    cheats[cheatIndex].original[lineIndex] = newValue;
+  }
+
+  function addLineToCode(cheatIndex) {
+    cheats[cheatIndex].original.push('');
+    renderCheats();
+  }
+
+  function removeLineFromCode(cheatIndex, lineIndex) {
+    cheats[cheatIndex].original.splice(lineIndex, 1);
+    renderCheats();
   }
 
   function convertCode(cheat, index) {
-    if(frontPatterns.some(p => cheat.original.substring(0, 3) === p)) {
-      const splitCode = cheat.original.split(' ');
-      const part1 = splitCode[0].substring(3);
-      const part2 = splitCode[1].replace(/^0+/, '').match(/.{1,2}/g).reverse().join(',');
-      cheats[index].converted = `${part1},${part2}`;
+    cheat.original.forEach(function(line, lineIndex) {
+      if(frontPatterns.some(p => line.substring(0, 3) === p)) {
+        const splitCode = line.split(' ');
+        const part1 = splitCode[0].substring(3);
+        const part2 = splitCode[1].replace(/^0+/, '').match(/.{1,2}/g).reverse().join(',');
+        cheats[index].converted += `${part1},${part2}`;
+        if(lineIndex < cheat.original.length - 1) {
+          cheats[index].converted += ';';
+        }
+      } else {
+        console.log(`Cheat ${cheat.name} - Line ${lineIndex} is invalid`);
+        alert('Looks like the code you entered is invalid!\nMake sure to check the rules on how codes should start');
+      }
+    });
 
-      renderCheats();
-    } else {
-      console.log('Cheat is invalid');
-      alert('Looks like the code you entered is invalid!\nMake sure to check the rules on how codes should start');
-    }
+    renderCheats();
+  }
+
+  function createInputs(originals, cheatIndex, parent) {
+    originals.forEach((cheat, index) => {
+      const inputLabel = document.createElement('label');
+      inputLabel.classList.add('label');
+      inputLabel.innerText = `Line ${index + 1}`;
+      const lineDelete = document.createElement('button');
+      lineDelete.classList.add('delete');
+      lineDelete.classList.add('is-pulled-right');
+      lineDelete.addEventListener('click', function() { removeLineFromCode(cheatIndex, index) });
+      inputLabel.appendChild(lineDelete);
+      const inputDiv = document.createElement('div');
+      inputDiv.classList.add('control');
+      inputDiv.classList.add('is-expanded');
+      const cheatInput = document.createElement('input');
+      cheatInput.classList.add('input');
+      cheatInput.value = cheat;
+      cheatInput.addEventListener('input', function(e) { onCodeLineChange(e.target.value, cheatIndex, index) });
+
+      inputDiv.appendChild(cheatInput);
+      parent.appendChild(inputLabel);
+      parent.appendChild(inputDiv);
+    });
   }
 
   function renderCheats() {
@@ -45,46 +84,47 @@
     cheats.forEach((cheat, index) => {
       const divField = document.createElement('div');
       divField.classList.add('field');
-      divField.classList.add('has-addons');
+
       const deleteButton = document.createElement('button');
       deleteButton.classList.add('delete');
       deleteButton.classList.add('is-large');
       deleteButton.addEventListener('click', function() { removeCheat(index); });
-      const inputLabel = document.createElement('label');
-      inputLabel.classList.add('label');
-      const inputDiv = document.createElement('div');
-      inputDiv.classList.add('control');
-      inputDiv.classList.add('is-expanded');
-      const cheatInput = document.createElement('input');
-      cheatInput.classList.add('input');
-      cheatInput.value = cheat.original;
-      cheatInput.addEventListener('input', function(e) { onCodeChange(e.target.value, index) });
+      
       const buttonDiv = document.createElement('div');
       buttonDiv.classList.add('control');
+
       const convertButton = document.createElement('button');
       convertButton.innerText = 'Convert Code';
       convertButton.classList.add('button');
       convertButton.addEventListener('click', function() { convertCode(cheat, index); });
 
+      const addLineButton = document.createElement('button');
+      addLineButton.innerText = 'Add Line to Code';
+      addLineButton.classList.add('button');
+      addLineButton.addEventListener('click', function() { addLineToCode(index) });
+
       const divBox = document.createElement('div');
       divBox.classList.add('box');
-      divBox.innerText = `
-        Converted Cheat: ${cheat.converted}
-        .CHT Line for copy/paste: ON=${cheat.converted}
+      divBox.innerText = `Converted Cheat: ${cheat.converted}
+        .CHT Lines for copy/paste:
+        
+        [${cheat.name}]
+        ON=${cheat.converted}
       `;
 
       const articleMessage = document.createElement('article');
       articleMessage.classList.add('message');
+
       const messageHeader = document.createElement('div');
       messageHeader.classList.add('message-header');
       messageHeader.innerText = cheat.name;
+
       const messageBody = document.createElement('div');
       messageBody.classList.add('message-body');
 
-      inputDiv.appendChild(cheatInput);
+      buttonDiv.appendChild(addLineButton);
       buttonDiv.appendChild(convertButton);
-      divField.appendChild(inputLabel);
-      divField.appendChild(inputDiv);
+      createInputs(cheat.original, index, divField);
       divField.appendChild(buttonDiv);
 
       messageBody.appendChild(divField);
