06d9ec007afe815c8f5ecd11a78559146b45f7c7
Sean Robertson <srobertson421@gmail.com>
2020-03-08 21:37:51 -0700

    Added pwa icons and manifest. Added logo. Added redirects rule for Netlify

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,13 +4,18 @@ import {
   Title,
   Button,
   Icon,
+  Image
 } from 'beautiful-react-ui'
+import logo from '../../assets/cover.png';
 
 const Home = ({ user, history }) => {
   return (
     <Grid itemsAlign="center">
       <Grid.Column size="12">
-        <Title textAlign="center">{user ? `Welcome ${user.email} To` : null} CHT DB</Title>
+        <Image src={logo} alt="CHT Database Logo" />
+        { user && (
+          <Title textAlign="center">Welcome ${user.email}</Title>
+        ) }
       </Grid.Column>
       { !user && (
         <>
@@ -43,6 +48,15 @@ const Home = ({ user, history }) => {
         <Title textAlign="center" tagName="h3">Check out the existing DB of .CHT Files</Title>
         <Grid.Column size="6" offset="3">
           <Button fluid color="primary" onClick={() => history.push('/cheats')}>Cheat DB <Icon name="arrow-right" /></Button>
+        </Grid.Column>
+      </Grid.Column>
+      <Grid.Column size="12">
+        <Title tagName="h5" textAlign="center">OR</Title>
+      </Grid.Column>
+      <Grid.Column size="12" md="10" offsetMd="1" lg="8" offsetLg="2" xl="8" offsetXl="2">
+        <Title textAlign="center" tagName="h3">Check out the existing CHT content from EZ-Flash</Title>
+        <Grid.Column size="6" offset="3">
+          <Button fluid color="primary" onClick={() => history.push('/ez-flash-cheats')}>EZ-Flash Cheats <Icon name="arrow-right" /></Button>
         </Grid.Column>
       </Grid.Column>
     </Grid>
